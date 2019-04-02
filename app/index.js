c95981bce8d0c83b4ea969a64e2e6b8b475d2ac0
Wen Peng Guo <Peng_W_Guo@manulife.com>
2019-04-02 11:04:05 +0800

    Modify base url

--- app/index.js
+++ app/index.js
@@ -227,25 +227,25 @@ var getIconStyle = function(type) {
     switch (type.toLowerCase()) {
         case 'default':
             style.color = 'DARKTURQUOISE';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Objects.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Objects.png';
             style.size = '63%';
             break;
         case 'story':
         case 'user story':
             style.color = 'GOLD';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Bulb.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Bulb.png';
             style.size = '63%';
             break;
         case 'bug':
         case 'problem':
         case 'correction':
             style.color = 'CRIMSON';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Bug.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Bug.png';
             style.size = '63%';
             break;
         case 'epic':
             style.color = 'ROYALBLUE';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Flash.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Flash.png';
             style.size = '63%';
             break;
         case 'task':
@@ -255,28 +255,28 @@ var getIconStyle = function(type) {
         case 'unteraufgabe':
         case 'technische aufgabe':
             style.color = 'WHEAT';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Task.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Task.png';
             style.size = '63%';
             break;
         case 'new feature':
             style.color = 'LIMEGREEN';
-            style.image = "https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Plus.png";
+            style.image = "https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Plus.png";
             style.size = '63%';
             break;
         case 'improvement':
         case 'verbesserung':
             style.color = 'CORNFLOWERBLUE';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/Arrow.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/Arrow.png';
             style.size = '63%';
             break;
         case 'research':
             style.color = 'MEDIUMTURQUOISE';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/ErlenmeyerFlask.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/ErlenmeyerFlask.png';
             style.size = '63%';
             break;
         case 'test':
             style.color = 'ORANGE';
-            style.image = 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/icons/CrashDummy.png';
+            style.image = 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/icons/CrashDummy.png';
             style.size = '63%';
             break;
     }
@@ -420,7 +420,7 @@ var renderCards = function(issueKeyList) {
 
     $("head", printFrameDocument).append($('<style>').html(fs.readFileSync(__dirname + '/card.css', 'utf8')));
     // preload some resources
-    $("head", printFrameDocument).append($('<link rel="subresource"/>').attr('href', 'https://qoomon.github.io/Jira-Issue-Card-Printer/resources/Tearing.png'));
+    $("head", printFrameDocument).append($('<link rel="subresource"/>').attr('href', 'https://github.com/smallg/Jira-Issue-Card-Printer/dist/resources/Tearing.png'));
 
     console.log("load " + issueKeyList.length + " issues...");
 
